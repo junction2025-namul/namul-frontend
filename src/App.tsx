add7461ee789577b02f0b38c8558cdeb058ecac6
hyewonimdang <hyehyekuk202@gmail.com>
2025-08-23 11:30:30 +0900

    basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ErrorView from './components/common/ErrorView';
+import Layout from './layout/Layout';
 
 function App() {
+  const queryClient = new QueryClient();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorView />,
+    },
+  ]);
 
   return (
-    <>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App
---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => {
+    return (
+        <></>
+    );
+};
+
+export default LoadingSpinner;
