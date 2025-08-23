67b9826748566f52c11d41b3b6f9567392bb77bd
hyewonimdang <hyehyekuk202@gmail.com>
2025-08-24 01:18:56 +0900

    develop test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' // develop test
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -15,3 +15,4 @@ export default defineConfig({
        },
      },
 })
+
